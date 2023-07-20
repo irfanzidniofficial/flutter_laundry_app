7a2c1ac38710be3fb52b2f8815d47483706940e8
irfanzidniofficial <irfanzidniofficial@gmail.com>
2023-07-20 16:12:02 +0700

    laundry view page integration api

--- lib/pages/dashboard_view/my_laundry_view.dart
+++ lib/pages/dashboard_view/my_laundry_view.dart
@@ -1,11 +1,17 @@
+import 'package:d_input/d_input.dart';
+import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_laundry_app/config/app_constants.dart';
+import 'package:flutter_laundry_app/config/app_format.dart';
 import 'package:flutter_laundry_app/config/app_session.dart';
 import 'package:flutter_laundry_app/datasource/laundry_datasource.dart';
 import 'package:flutter_laundry_app/models/laundry_model.dart';
 import 'package:flutter_laundry_app/models/user_model.dart';
 import 'package:flutter_laundry_app/providers/my_laudry_provider.dart';
+import 'package:flutter_laundry_app/widgets/error_background.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 import '../../config/failure.dart';
 
@@ -56,7 +62,55 @@ class _MyLaundyViewState extends ConsumerState<MyLaundyView> {
     });
   }
 
-  dialogClaim() {}
+  dialogClaim() {
+    final edtLaundryID = TextEditingController();
+    final edtClaimCode = TextEditingController();
+    final formKey = GlobalKey<FormState>();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Form(
+          child: SimpleDialog(
+            titlePadding: const EdgeInsets.all(16),
+            contentPadding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            children: [
+              DInput(
+                controller: edtLaundryID,
+                title: 'Laundry ID',
+                radius: BorderRadius.circular(10),
+                validator: (input) => input == '' ? "Don't empty" : null,
+                inputType: TextInputType.number,
+              ),
+              DView.spaceHeight(),
+              DInput(
+                controller: edtClaimCode,
+                title: 'Claim Code',
+                radius: BorderRadius.circular(10),
+                validator: (input) => input == '' ? "Don't empty" : null,
+                inputType: TextInputType.number,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    claimNow();
+                  }
+                },
+                child: const Text("Claim Now"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  claimNow() {}
 
   @override
   void initState() {
@@ -71,36 +125,271 @@ class _MyLaundyViewState extends ConsumerState<MyLaundyView> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 60, 30, 20),
-              child: Row(
-                children: [
-                  Text(
-                    "My Laundry",
-                    style: GoogleFonts.montserrat(
-                      fontSize: 24,
-                      color: Colors.green,
-                      fontWeight: FontWeight.w600,
+        header(),
+        categories(),
+        Expanded(
+          child: RefreshIndicator(
+            onRefresh: () async => getMyLaundry(),
+            child: Consumer(
+              builder: (_, wiRef, ___) {
+                String statusList = wiRef.watch(myLaundryCategoryProvider);
+                String statusCategory = wiRef.watch(myLaundryCategoryProvider);
+
+                List<LaundryModel> listBackup =
+                    wiRef.watch(myLaundryListProvider);
+
+                if (statusList == '') return DView.loadingCircle();
+                if (statusList != "Success") {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 0, 30, 80),
+                    child: ErrorBackground(ratio: 16 / 9, message: statusList),
+                  );
+                }
+
+                List<LaundryModel> list = [];
+                if (statusCategory == 'All') {
+                  list = List.from(listBackup);
+                } else {
+                  list = listBackup
+                      .where((element) => element.status == statusCategory)
+                      .toList();
+                }
+
+                if (list.isEmpty) {
+                  return const Padding(
+                    padding: EdgeInsets.fromLTRB(30, 0, 30, 80),
+                    child: ErrorBackground(
+                      ratio: 16 / 9,
+                      message: 'Empty',
                     ),
-                  ),
-                  OutlinedButton.icon(
-                    onPressed: () => dialogClaim(),
-                    label: const Text(
-                      "Claim",
-                      style: TextStyle(
-                        height: 1,
+                  );
+                }
+                return GroupedListView<LaundryModel, String>(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 80),
+                  elements: list,
+                  itemComparator: (element1, element2) {
+                    return element1.createdAt.compareTo(element2.createdAt);
+                  },
+                  groupSeparatorBuilder: (value) => Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(
+                          color: Colors.grey[400]!,
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(
+                        top: 24,
+                        bottom: 20,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 4,
+                      ),
+                      child: Text(
+                        AppFormat.shortDate(value),
                       ),
                     ),
-                    icon: const Icon(Icons.add),
                   ),
-                ],
-              ),
+                  groupBy: (element) => AppFormat.justDate(element.createdAt),
+                  order: GroupedListOrder.DESC,
+                  itemBuilder: (context, laundry) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  laundry.shop.name,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                              DView.spaceWidth(),
+                              Text(
+                                AppFormat.longPrice(
+                                  laundry.total,
+                                ),
+                                textAlign: TextAlign.end,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                          DView.spaceHeight(12),
+                          Row(
+                            children: [
+                              if (laundry.withPickup)
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  margin: const EdgeInsets.only(right: 8),
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 4,
+                                  ),
+                                  child: const Text(
+                                    'Pickup',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      height: 1,
+                                    ),
+                                  ),
+                                ),
+                              if (laundry.withDelivery)
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  margin: const EdgeInsets.only(right: 8),
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 4,
+                                  ),
+                                  child: const Text(
+                                    'Delivery',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      height: 1,
+                                    ),
+                                  ),
+                                ),
+                              Expanded(
+                                child: Text(
+                                  '${laundry.weight}kg',
+                                  textAlign: TextAlign.end,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
             ),
-          ],
+          ),
         )
       ],
+    );
+  }
+
+  Consumer categories() {
+    return Consumer(
+      builder: (_, wiRef, ___) {
+        String categorySelected = wiRef.watch(myLaundryCategoryProvider);
+        return SizedBox(
+          height: 30,
+          child: ListView.builder(
+            itemCount: AppConstants.laundryStatusCategory.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              String category = AppConstants.laundryStatusCategory[index];
+              return Padding(
+                padding: EdgeInsets.only(
+                    left: index == 0 ? 30 : 8,
+                    right: index == AppConstants.laundryStatusCategory.length
+                        ? 30
+                        : 8),
+                child: InkWell(
+                  onTap: () {
+                    setMyLaundryCategory(
+                      ref,
+                      category,
+                    );
+                  },
+                  borderRadius: BorderRadius.circular(30),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: category == categorySelected
+                            ? Colors.green
+                            : Colors.grey[400]!,
+                      ),
+                      color: category == categorySelected
+                          ? Colors.green
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      category,
+                      style: TextStyle(
+                        height: 1,
+                        color: category == categorySelected
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Padding header() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30, 60, 30, 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "My Laundry",
+            style: GoogleFonts.montserrat(
+              fontSize: 24,
+              color: Colors.green,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          OutlinedButton.icon(
+            onPressed: () => dialogClaim(),
+            label: const Text(
+              "Claim",
+              style: TextStyle(
+                height: 1,
+              ),
+            ),
+            icon: const Icon(Icons.add),
+            style: ButtonStyle(
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                padding: const MaterialStatePropertyAll(
+                  EdgeInsets.fromLTRB(8, 2, 16, 2),
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
